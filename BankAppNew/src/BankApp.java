df7b7aabad0660300ea1e770cc94399135ddb6e1
AaronInt <ainthirath@umass.edu>
2021-08-12 17:00:42 -0400

    Implemented the deposit, withdraw, and current balance methods. Main functionality works so far. Figuring out what I should do for transaction history and how to make the app look better.

--- BankAppNew/src/BankApp.java
+++ BankAppNew/src/BankApp.java
@@ -1,6 +1,45 @@
+import java.util.ArrayList;
 
 public class BankApp {
-	public static void main(String[] args){
-		System.out.println("Hellodsdesgfdgfd github testing");
+	int balance;
+	String[] history = new String[500];
+	int index;
+	
+	public void deposit(int amount) {
+		balance = balance + amount;
+		System.out.println("You've chosen to deposit $" + amount + " from your bank account.");
+		System.out.println("You're current balance is now $" + balance + "." + "\n");
+		//history[index] = "Deposited $" + Integer.toString(amount);
+		//index++;
 	}
+	
+	public void withdraw(int amount) {
+		balance = balance - amount;
+		System.out.println("You've chosen to withdraw $" + amount + " from your bank account.");
+		System.out.println("You're current balance is now $" + balance + "." + "\n");
+		//history[index] = "Withdrew $" + Integer.toString(amount);
+		//index++;
+	}
+	
+	public void currentBalance() {
+		System.out.println("Your current balance is $" + balance + "." + "\n");
+	}
+	
+	/*
+	public void transactionHist() {
+		if(history.length == 0) {
+			System.out.println("There are no previous transactions." + "\n");
+		} else {
+			for(int i=history.length-1;i>=Math.max(0, history.length-3);i--) {
+				System.out.println(history[i]);
+			}
+		}
+	}
+	*/
+	
+	
+	
+	
+	
+	
 }
--- BankAppNew/src/InputScanner.java
+++ BankAppNew/src/InputScanner.java
@@ -1,17 +1,57 @@
 import java.util.Scanner;
 
-public class InputScanner {
+public class InputScanner extends BankApp {
+	
+	
+	
 	public static void main(String[] args) {
-		Scanner inputObj = new Scanner(System.in);
-		System.out.println("What would you like to do? (Please enter the number corresponding to the desired action)" + "\n");
+		displayMenu();
 		
-		System.out.println("(1) Deposit to your account");
-		System.out.println("(2) Withdraw from your account");
-		System.out.println("(3) Check your balance");
-		System.out.println("(4) Transaction History");
-		System.out.println("(5) Exit");
+	}
+	
+	public static void displayMenu() {
+			
+		BankApp currentUser = new BankApp();
+		String str_input = "";
 		
-		String str_input = inputObj.nextLine();
-		System.out.println("You would like to: " + str_input);
+		do {	
+			Scanner inputObj = new Scanner(System.in);
+			System.out.println("What would you like to do? (Please enter the number corresponding to the desired action)" + "\n");
+			
+			System.out.println("(A) Deposit to your account");
+			System.out.println("(B) Withdraw from your account");
+			System.out.println("(C) Check your balance");
+			//System.out.println("(D) Transaction History");
+			System.out.println("(D) Exit");
+			
+			str_input = inputObj.nextLine().toLowerCase();
+			
+			switch(str_input) {
+				case "a": 
+					System.out.println("How much do you want to deposit out of your bank account?");
+					Scanner depObj = new Scanner(System.in);
+					int dep_amount = depObj.nextInt();
+					currentUser.deposit(dep_amount);
+					break;
+				case "b": 
+					System.out.println("How much do you want to withdraw from your bank account?");
+					Scanner withObj = new Scanner(System.in);
+					int with_amount = withObj.nextInt();
+					currentUser.withdraw(with_amount);
+					break;
+				case "c": 
+					currentUser.currentBalance();
+					break;
+				/*	
+				case "d": 
+					System.out.println("Here is your transaction history:");
+					currentUser.transactionHist();
+					break;
+				*/	
+				default: System.out.println("You've chosen to exit. Have a good day!");
+					break;
+			}
+		} while(!str_input.equals("d"));	
+		
 	}
 }
